ff08afad53eca7b457c7651c3e383d0d657ccefb
Abeshet <hiabeshet@gmail.com>
2023-02-17 22:37:12 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - size of data type
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char))
+	printf("Size of a int: %zu byte(s)\n", sizeof(int))
+	printf("Size of a float: %zu byte(s)\n", sizeof(float))
+    return (0);
+}
