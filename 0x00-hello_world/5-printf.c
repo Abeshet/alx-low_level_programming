b92e591d627fee340433a2ce8028ee025752886d
Abeshet <hiabeshet@gmail.com>
2023-02-17 21:55:20 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 /**
  * main -fuction print with proper grammar,
  * but the outcome is a piece of art,with prinf 
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-      	 printf("with proper grammar, but the outcome is a piece of art,\n");
-         return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
