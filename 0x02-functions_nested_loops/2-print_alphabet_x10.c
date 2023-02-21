fddd03b129987f51981919c1d69c7dae4c34a2d9
Abeshet <hiabeshet@gmail.com>
2023-02-21 06:48:18 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10 time
+ */
+void print_alphabet_x10(void)
+{
+	int a = 0;
+	char letter;
+
+	while (a++ <= 9)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+		_putchar('\n');
+	}
+}
