3a2c6b871944c3ec578a33115c6fa080c19ed36e
Abeshet <hiabeshet@gmail.com>
2023-02-23 07:45:20 -0800

    task 2 and 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_numbers - print number
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+}
+
