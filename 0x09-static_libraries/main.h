4d236a41c0e29676f6016de4328308476e8c5fbc
Abeshet <hiabeshet@gmail.com>
2023-03-11 22:09:33 -0800

    task 1

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -13,8 +11,8 @@ void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
+char *strncat(char *dest, char *src, int n);
+char *strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
